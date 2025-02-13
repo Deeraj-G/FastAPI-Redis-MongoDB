26cf4620482ae957e8b6c6f7fb95037e291e6ce4
Deeraj-G <djgurram@gmail.com>
2025-02-13 13:27:35 -0800

    Added helper function to convert str redis_id to bson binary for nsert to mongodb

--- helpers.py
+++ helpers.py
@@ -15,18 +15,15 @@ load_dotenv()
 
 MONGODB_URL = os.getenv("MONGODB_URL")
 
-
 # Establish the mongodb client
 def get_mongo_client():
     client = AsyncIOMotorClient(MONGODB_URL)
     return client
 
-
-# Get the
+# Get the 
 def get_db_from_mongo_client(mongo_client: AsyncIOMotorClientSession, db_name: str):
     db = mongo_client.get_database(db_name)
     return db
-
 
 # Simplify the return
 async def send_response(
@@ -43,11 +40,10 @@ async def send_response(
     # return the JSONResponse object
     return JSONResponse(content=content, status_code=status_code)
 
-
-# Convert the item.redis_id from given str type to bson Binary to insert into db with validation rule
+# Convert str type to bson Binary to insert into db with validation rule
 def convert_to_bson_binary(item: Item):
     item_dict = item.model_dump(by_alias=True)
-
+    
     item_dict["redis_id"] = Binary(UUID(item.redis_id).bytes, UUID_SUBTYPE)
 
     return item_dict

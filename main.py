da59f7485eb6a426a6eefcf0f62cf5e637823605
Deeraj-G <djgurram@gmail.com>
2025-02-14 16:35:41 -0800

    Need to implement redis_client changes with arq library

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
-from fastapi import FastAPI, Depends, status, Response
-from dotenv import load_dotenv
 from uuid import UUID
+
+from arq import ArqRedis, create_pool
+from arq.jobs import Job, JobStatus
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, Response, status
 from loguru import logger
 
-
-from models import Item, Collection
-from helpers import (
-    get_mongo_client,
-    get_db_from_mongo_client,
-    send_response,
-    convert_to_bson_binary,
-)
+from arq_worker import REDIS_SETTINGS
+from helpers import (convert_to_bson_binary, get_db_from_mongo_client,
+                     get_mongo_client, send_response)
+from models import Collection, Item
 from redis_pool import RedisPoolProvider
 
 load_dotenv()
@@ -44,7 +43,6 @@ async def get_entries(
             "status": response.status_code,
         }
 
-        # return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -60,7 +58,6 @@ async def get_entries(
             "status": status.HTTP_400_BAD_REQUEST,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -88,7 +85,6 @@ async def create_collection(
             "status": response.status_code,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -117,7 +113,6 @@ async def create_collection(
             "status": response.status_code,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -133,7 +128,6 @@ async def create_collection(
             "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -165,7 +159,6 @@ async def create_item(
             "status": response.status_code,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -182,7 +175,6 @@ async def create_item(
             "status": response.status_code,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -199,7 +191,6 @@ async def create_item(
             "status": response.status_code,
         }
 
-        # publish to the redis client and return a JSONObject
         return await send_response(
             content=response_data,
             status_code=response.status_code,
@@ -226,7 +217,6 @@ async def create_item(
         "status": response.status_code,
     }
 
-    # publish to the redis client and return a JSONObject
     return await send_response(
         content=response_data,
         status_code=response.status_code,
@@ -234,71 +224,3 @@ async def create_item(
         redis_client=redis_client,
         redis_key=f"{UUID(item.redis_id)}:post:items",
     )
-
-
-# @app.post("/post/items")
-# async def create_item(
-#     item: Item,
-#     response: Response,
-#     redis_client: RedisPoolProvider = Depends(RedisPoolProvider),
-# ):
-#     """
-#     Create an item in the specified db and collection
-#     """
-
-#     # get MongoDB client and establish connection with specified db
-#     db_conn = get_db_from_mongo_client(
-#         mongo_client=get_mongo_client(), db_name=item.db_name
-#     )
-
-#     if not item.redis_id:
-#         response.status_code = status.HTTP_400_BAD_REQUEST
-#         response_data = {
-#             "message": "POST Unsuccessful - redis_id invalid",
-#             "status": response.status_code,
-#         }
-#         return JSONResponse(content=response_data, status_code=response.status_code)
-
-#     # item wasn't passed in with collection as a valid param
-#     if not item.collection_name:
-#         response.status_code = status.HTTP_400_BAD_REQUEST
-#         response_data = {
-#             "message": f"POST Unsuccessful - collection '{item.collection_name}' does not exist",
-#             "status": response.status_code,
-#         }
-#         await publish(
-#             redis_client, f"{UUID(item.redis_id)}:post:items", json.dumps(response_data)
-#         )
-#         return JSONResponse(content=response_data, status_code=response.status_code)
-
-#     # collection doesn't exist
-#     if item.collection_name not in await db_conn.list_collection_names():
-#         response.status_code = status.HTTP_404_NOT_FOUND
-#         response_data = {
-#             "message": f"POST Unsuccessful - collection '{item.collection_name}' does not exist",
-#             "status": response.status_code,
-#         }
-#         await publish(
-#             redis_client, f"{UUID(item.redis_id)}:post:items", json.dumps(response_data)
-#         )
-
-#     # get the collection from MongoDB
-#     db_collection = db_conn.get_collection(f"{item.collection_name}")
-
-#     # handle insertion of item into specific collection
-#     new_item = await db_collection.insert_one(item.model_dump(by_alias=True))
-
-#     # find the _id of newly created item
-#     created_item = await db_collection.find_one({"_id": new_item.inserted_id})
-
-#     response.status_code = status.HTTP_201_CREATED
-#     response_data = {
-#         "message": f"POST Successful - created entry for item: {item.name} with item_id: {created_item['_id']}",
-#         "status": response.status_code,
-#     }
-
-#     await publish(
-#         redis_client, f"{UUID(item.redis_id)}:post:items", json.dumps(response_data)
-#     )
-
-#     return JSONResponse(content=response_data, status_code=response.status_code)
